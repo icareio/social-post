1ddc147896a2b5b09205a7a9140a7a79c7079d93
icareio <100024650+icareio@users.noreply.github.com>
2022-03-07 06:00:37 +0100

    feature/2_enableBuildContinuous (#3)
    * chore(2): create build continuous pipeline
    
    * chore(2): use task test instead of build and setup junit with assertj
    
    * chore(2): add jacoco report plugin and coverage badge
    
    * chore(2): remove info display for test step
    
    * chore(2): display coverage percent in build
    
    * chore(2): remove jacoco step
    
    Co-authored-by: ksudreau <kevin.sudreau+development@gmail.com>

---
+++ src/test/java/org/icareio/social/post/ApplicationTest.java
@@ -0,0 +1,17 @@
+package org.icareio.social.post;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Application main test class")
+class ApplicationTest {
+
+    @Test
+    @DisplayName("Test should compile")
+    void shouldCompile() {
+        assertThat(true).isTrue();
+    }
+
+}
